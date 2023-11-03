123a6fc1ad063b87de502f7c28b1061c14f39abd
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-11-03 15:59:25 +0800

    Update App.js
    added listening of deposit events

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ScStats from "./components/ScStats";
-import { useAccount, useWalletClient } from "wagmi";
+import { useAccount, useContractEvent } from "wagmi";
 import UserFeatures from "./components/UserFeatures";
+import smartContract from "./artifacts/contracts/MultiSigWallet.sol/MultiSigWallet.json";
 
 function App() {
   const { address, isConnected } = useAccount();
-  // const { data: walletClient, isSuccess } = useWalletClient();
+
+  useContractEvent({
+    address: process.env.REACT_APP_SC_ADDRESS,
+    abi: smartContract.abi,
+    eventName: "Deposit",
+    listener(log) {
+      console.log("Deposit: ", log[0].args);
+    },
+  });
 
   return (
     <div>
