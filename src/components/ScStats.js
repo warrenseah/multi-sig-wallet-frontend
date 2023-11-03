88f5b82e55433a9fa12a9adbb7ead00218217c46
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-11-03 16:00:38 +0800

    Update ScStats.js
    Added multiple reads for the smart contract

--- src/components/ScStats.js
+++ src/components/ScStats.js
@@ -1,28 +1,66 @@
-import { useBalance } from "wagmi";
+import { useContractReads } from "wagmi";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import contractABI from "../artifacts/contracts/MultiSigWallet.sol/MultiSigWallet.json";
+import { useEffect, useMemo, useState } from "react";
+import { formatEther } from "viem";
+
+const smartContract = {
+  address: process.env.REACT_APP_SC_ADDRESS,
+  abi: contractABI.abi,
+};
 
 function ScStats() {
-  const { data, isError, isLoading } = useBalance({
-    address: process.env.REACT_APP_SC_ADDRESS,
-    watch: true,
+  const [quorem, setQuorem] = useState();
+  const [owners, setOwners] = useState([]);
+  const [balanceOf, setBalanceOf] = useState(0);
+
+  const { data } = useContractReads({
+    contracts: [
+      {
+        ...smartContract,
+        functionName: "balanceOf",
+        watch: true,
+      },
+      {
+        ...smartContract,
+        functionName: "getOwners",
+      },
+      {
+        ...smartContract,
+        functionName: "quoremRequired",
+      },
+    ],
   });
 
-  const balanceFetching = () => {
-    if (isError) {
-      return <span>Error fetching balance</span>;
+  const obj = useMemo(() => {
+    if (data) return data;
+  }, [data]);
+
+  useEffect(() => {
+    if (obj) {
+      setBalanceOf(obj[0].result);
+      setOwners(obj[1].result);
+      setQuorem(obj[2].result);
+
+      console.log("Use effect renders ...");
     }
-    if (isLoading) {
-      return <span>Fetching balance ...</span>;
-    }
-    if (data) {
-      return <span>{`${data.formatted} ${data.symbol}`}</span>;
-    }
-  };
+  }, [obj]);
 
   return (
     <>
-      <h1>Smart Contract Stats</h1>
-      <p>Contract Address: {process.env.REACT_APP_SC_ADDRESS}</p>
-      <p>Balance: {balanceFetching()}</p>
+      <Row>
+        <Col>
+          <h1>Smart Contract Stats</h1>
+          <p>Contract Address: {process.env.REACT_APP_SC_ADDRESS}</p>
+          <p>Balance: {balanceOf && `${formatEther(balanceOf)} Eth`}</p>
+          <p>Quorem: {quorem && quorem.toString()}</p>
+          <div>Owners</div>
+          <ul>
+            {owners && owners.map((owner) => <li key={owner}>{owner}</li>)}
+          </ul>
+        </Col>
+      </Row>
     </>
   );
 }
