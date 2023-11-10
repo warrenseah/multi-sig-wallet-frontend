78831870f25ab6e8ac7cc8f933429e9e8928a53d
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-11-10 14:35:04 +0800

    Update FactoryActions.js

--- src/components/FactoryActions.js
+++ src/components/FactoryActions.js
@@ -1,6 +1,6 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   useContractEvent,
   useContractWrite,
