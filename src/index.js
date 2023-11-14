00c85ad2354f7bb870ff279d5719037fe4fafd0c
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-11-14 19:30:06 +0800

    added new wallets

--- src/index.js
+++ src/index.js
@@ -6,7 +6,22 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  injectedWallet,
+  rainbowWallet,
+  walletConnectWallet,
+  ledgerWallet,
+  coinbaseWallet,
+  metaMaskWallet,
+  okxWallet,
+  trustWallet,
+  tokenPocketWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { mainnet, sepolia, hardhat, bscTestnet } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
@@ -20,11 +35,40 @@ const { chains, publicClient } = configureChains(
   ]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  projectId: "e7bff16076569fced20d29ffb3a853e6",
-  chains,
-});
+const projectId = process.env.REACT_APP_PROJECTID;
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      injectedWallet({ chains }),
+      metaMaskWallet({ projectId, chains }),
+      ledgerWallet({
+        projectId,
+        chains,
+        infuraId: process.env.REACT_APP_INFURAID,
+      }),
+      rainbowWallet({ projectId, chains }),
+      walletConnectWallet({
+        projectId,
+        chains,
+      }),
+      coinbaseWallet({ appName: "My RainbowKit App", chains }),
+      okxWallet({ projectId, chains }),
+      tokenPocketWallet({
+        projectId,
+        chains,
+      }),
+      trustWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+// const { connectors } = getDefaultWallets({
+//   appName: "My RainbowKit App",
+//   projectId,
+//   chains,
+// });
 
 const wagmiConfig = createConfig({
   autoConnect: true,
